a05de2290ba8cd7f17338a0cc32869bc45c5cc99
Clayton Kerce <clayton.kerce@gtri.gatech.edu>
2022-05-28 09:22:43 -0400

    Added command line argument parsing.

--- run_scan.sh
+++ run_scan.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-#bssid=$1
+N='NULL'
+while (( "$#" )); do
+  case "$1" in
+    -n|--num_scans)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        N=$2
+        shift 2
+	echo "Setting number of scans N=$N"
+      else
+	N=1000
+	echo "Using default number of scans N=1000"
+      fi
+  esac
+done
 
-#nmcli dev wifi list bssid $bssid
+if [ 'NULL' == $N ] ; then
+	N=1000
+	echo "Using default number of scans N=1000"
+fi
+
+
 
 outfile='nmcli_scan.txt'
 datfile='outdat.csv'
@@ -10,7 +28,7 @@ echo '' > $outfile
 echo 'dat,ssid,val,,,' > $datfile
 
 
-for i in $(seq 1 1000)
+for i in $(seq 1 $N)
 do
    echo Scan $i
    ts=$(date +%s)
@@ -24,4 +42,4 @@ done
 #cat $datfile | sed -e 's/,,,//g' >> mod_$datfile
 num=$(ls -lahtr working*.csv | wc -l)
 workingfile=working_$num.csv
-cat $datfile | sed -e 's/,,,/,/g' -e 's/,,,/,/g' -e 's/,,/,/g' -e 's/,,/,/g' -e 's/,$//g' > $workingfile
+cat $datfile | sed -e 's/,,,/,/g' -e 's/,,,/,/g' -e 's/,,/,/g' -e 's/,,/,/g' -e 's/,$//g' -e 's/_,/_/g' > $workingfile
